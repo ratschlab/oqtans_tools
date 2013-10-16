a69b5b45f4e94dedd0d9cb47ae121d1f265e26ac
Vipin <vipin at cbio.mskcc.org>
2013-10-16 14:34:30 -0400

    master setup script updated the programs to be compiled

--- oqtans_make.sh
+++ oqtans_make.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-
+#
+# oqtans submodule compile script
+# 
 set -e
 
 basedir=`dirname $0`
 if [ "$basedir" == "." ];
-then 
+then
     basedir=`pwd`/
 fi
 
 echo $basedir
 
-for i in palmapper/0.5 DESeq/1.12 DESeq/2-1.0.18 rDiff/0.1 rQuant/2.2 mTIM/0.2 trinity/r2012-06-08 bwa/0.6.2 EasySVM/0.3.3
+for i in PALMapper/0.5 DESeq/1.12 DESeq2/1.0.19 rQuant/2.2 mTIM/0.2 Trinity/r2013_08_14 BWA/0.6.2 edgeR/0.2 EasySVM/0.3.3 rDiff/0.3
 do
     cd $basedir/$i
     echo ==============================================================
     echo ===================== making $i ===============
     echo ==============================================================
     ./oqtans_make.sh $1
+    echo ================== created binary files for $i ===============
 done
-
-
